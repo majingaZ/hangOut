937964dc3a274aab118cfa9f5ba82d4098a50dad
heeja <heejaewg@gmail.com>
2024-02-16 12:18:11 +0900

    Initial commit

---
+++ src/main/java/com/hangout/HangOutApplication.java
@@ -0,0 +1,13 @@
+package com.hangout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangOutApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(HangOutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hangout/HangOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangOutApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
